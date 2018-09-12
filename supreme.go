8bdf4a040acd7394e93f7ea8876a0c1598ceb09e
yuda <akuntestinguntukseto@gmail.com>
2018-09-12 08:55:43 +0700

    try with parameter

--- supreme.go
+++ supreme.go
@@ -5,10 +5,23 @@ import (
 	"bytes"
 	"io/ioutil"
 	"fmt"
+	"encoding/json"
+	"github.com/graphql-go/graphql"
+	"log"
 )
 
-func main() {
-	var jsonValue = []byte(`{"query":""query {↵  userById(id:100) {↵    id↵  }↵}","variables":{}}`)
+func graph(query string) {
+	params := graphql.Params{RequestString: query}
+	r := graphql.Do(params)
+	if len(r.Errors) > 0 {
+		log.Fatalf("failed to execute graphql operation, errors: %+v", r.Errors)
+	}
+	rJSON, _ := json.Marshal(r)
+	fmt.Printf("%s \n", rJSON)
+}
+
+func bisa() {
+	var jsonValue = []byte(`{"query":"query {\n  userById(id:100) {\n    id\n  }\n}","variables":null}`)
 	resp, err := http.Post("https://graphql.tebengan.id/graphql", "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
 		panic(err)
@@ -20,4 +33,25 @@ func main() {
   fmt.Println("response Headers:", resp.Header)
   body, _ := ioutil.ReadAll(resp.Body)
   fmt.Println("response Body:", string(body))
+}
+
+func message(query string, variables string) string {
+	values := map[string]string{"query": query, "variables": variables}
+	jsonValue, _ := json.Marshal(values)
+	fmt.Println("jsonValue", string(jsonValue))
+	resp, err := http.Post("https://graphql.tebengan.id/graphql", "application/json", bytes.NewBuffer(jsonValue))
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+	return string(body)
+}
+
+func main() {
+	//bisa()
+	fmt.Println(message("query {\n  userById(id:100) {\n    id\n  }\n}","{}"))
+	//graph("query {\n  userById(id:100) {\n    id\n  }\n}")
 }
